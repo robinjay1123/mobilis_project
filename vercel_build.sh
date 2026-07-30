7d3f6812d2616c6a571d1726f9628708e6ec8049
Robin Jay Banaag <23sc4129_ms@psu.edu.ph>
2026-07-30 22:25:45 +0800

    Set PUB_CACHE to workspace in vercel_build.sh for permission fix

--- vercel_build.sh
+++ vercel_build.sh
@@ -2,12 +2,14 @@
 set -e
 
 echo "=== Cleaning old Flutter cache ==="
-rm -rf flutter
+rm -rf flutter .pub-cache .dart_tool
+
+echo "=== Setting Environment Variables ==="
+export PUB_CACHE="$PWD/.pub-cache"
+export PATH="$PWD/flutter/bin:$PATH"
 
 echo "=== Downloading Flutter 3.27.0 Linux SDK ==="
 curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.0-stable.tar.xz | tar -xJ
-
-export PATH="$PWD/flutter/bin:$PATH"
 
 echo "=== Flutter SDK Info ==="
 flutter --version
