a5b60451964f655e8a3c38b1dfbb84a306be3cc7
Robin Jay Banaag <23sc4129_ms@psu.edu.ph>
2026-08-07 23:29:56 +0800

    Purge cached Flutter SDK before cloning 3.27.4 in vercel_install.sh

--- vercel_install.sh
+++ vercel_install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
 git config --global --add safe.directory '*'
-if [ ! -d "flutter" ]; then
-  curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.0-stable.tar.xz | tar -xJ
+if [ -d "flutter" ]; then
+  rm -rf flutter
 fi
+git clone --depth 1 --branch 3.27.4 https://github.com/flutter/flutter.git
 ./flutter/bin/flutter config --enable-web
 ./flutter/bin/flutter pub get
