10398ecfaffe60d43ad8c702b5403f4baed9a72b
Robin Jay Banaag <23sc4129_ms@psu.edu.ph>
2026-08-07 23:34:08 +0800

    Revert Vercel config back to original working state

--- vercel_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-git config --global --add safe.directory '*'
-if [ -d "flutter" ]; then
-  rm -rf flutter
-fi
-git clone --depth 1 --branch 3.27.4 https://github.com/flutter/flutter.git
-./flutter/bin/flutter config --enable-web
-./flutter/bin/flutter pub get
